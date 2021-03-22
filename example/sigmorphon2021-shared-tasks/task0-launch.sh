b75a443079a851c9a81765473a30e5134b67ab5e
Tiago Pimentel <tiagopms@gmail.com>
2021-03-22 18:19:08 -0300

    Fix train all script

--- example/sigmorphon2021-shared-tasks/task0-launch.sh
+++ example/sigmorphon2021-shared-tasks/task0-launch.sh
@@ -1,11 +1,12 @@
 run=bash
 
-for lng in afb ame ara aym bul ckb cni evn heb itl kod lud nld pol rus see syc vep ail amh arz bra ces ckt deu gup ind kmr krl mag olo por sah spa tyv
+for lang in afb ame ara aym bul ckb cni evn heb itl kod lud nld pol rus see syc vep ail amh arz bra ces ckt deu gup ind kmr krl mag olo por sah spa tyv
+do
     # regular data
-    echo trm $lang
+    echo "trm" $lang
     $run baselines/neural/example/sigmorphon2021-shared-tasks/task0-trm.sh $lang
 
     # augmented data
-    echo trm hall $lang
+    echo "trm hall" $lang
     $run baselines/neural/example/sigmorphon2021-shared-tasks/task0-hall-trm.sh $lang
 done
